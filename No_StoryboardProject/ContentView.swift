ef9e412fba2866d9476296029c240e4b7509e105
Anthony Beckford <abeckford532@gmail.com>
2022-09-09 16:01:31 -0400

    Project setup without Storyboard

---
+++ No_StoryboardProject/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  No_StoryboardProject
+//
+//  Created by Anthony Beckford on 9/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
